e5097b33eae6890a1cc9d2b2cf285af599039f03
animeshbiyani <35753548+animeshbiyani@users.noreply.github.com>
2018-06-16 19:58:40 +0530

    Update priority_queue_binary_heap.py

--- data structures/priority_queue_binary_heap.py
+++ data structures/priority_queue_binary_heap.py
@@ -7,9 +7,9 @@ class binHeap:
     while (i // 2) > 0:
       if self.heapList[i] < self.heapList[i // 2]:
         tmp = self.heapList[i]
-        self.heapList[i // 2] = self.heapList[i]
+        self.heapList[i] = self.heapList[i // 2]
         self.heapList[i // 2] = tmp
-    i = i // 2
+      i = i // 2
     
   def insert(self,k):
     self.heapList.append(k)
